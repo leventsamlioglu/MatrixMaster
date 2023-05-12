f5d37f251bfebe244befa9078a9f3f687fcab71c
leventsamlioglu <leventsamlioglu@icloud.com>
2023-05-12 20:52:12 +0200

    some arrangements

--- jQuery-AJAX/Assignments/Turing-Gates/index.js
+++ jQuery-AJAX/Assignments/Turing-Gates/index.js
@@ -1,9 +1,12 @@
-$("img").click(function() {
-    $(this).fadeOut(1000, function() {
-        $(this).attr("src",$(this).attr("alt-pic"));
-    }).fadeIn(1000);
+$("img").click(function () {
+	$(this)
+		.fadeOut(1000, function () {
+			var src = $(this).attr("src");
+			$(this).attr("src", $(this).attr("alt-pic"));
+			$(this).attr("alt-pic", src);
+		})
+		.fadeIn(1000);
 });
-
 
 // $("img").click(function() {
 //     $(this).slideUp(1000, function() {
@@ -11,6 +14,3 @@ $("img").click(function() {
 //     }).slideDown(1000);
 //     // return false;
 // });
-
-
-   
