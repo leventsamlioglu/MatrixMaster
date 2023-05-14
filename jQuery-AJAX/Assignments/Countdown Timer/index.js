4be2b70b7aa590239f46da6d812f766832310c38
leventsamlioglu <leventsamlioglu@icloud.com>
2023-05-14 19:10:42 +0200

    fix some bugs

--- jQuery-AJAX/Assignments/Countdown Timer/index.js
+++ jQuery-AJAX/Assignments/Countdown Timer/index.js
@@ -1,16 +1,16 @@
 let duration = $("#edit-number").text() * 60;
-
+let edit = false;
 let milli = 100;
 let timeout, timeout1;
 let timer_on = 0;
 var minutes, seconds, milliseconds;
 
 function timedCount() {
+	if (edit) return;
 	if (duration == 0) {
 		clearTimeout(timeout);
 		clearTimeout(timeout1);
 		duration = $("#edit-number").text() * 60;
-        
 		return;
 	}
 	duration--;
@@ -26,6 +26,7 @@ function timedCount() {
 }
 
 function countMili() {
+	if (edit) return;
 	milliseconds = milli < 10 ? "0" + milli : milli;
 	$("#timer1").text(milliseconds);
 	milli == 0 ? (milli = 100) : milli--;
@@ -44,15 +45,23 @@ $("#reset").click(function () {
 	clearTimeout(timeout);
 	clearTimeout(timeout1);
 	milli = 100;
-	duration = $("#edit-number").text() * 60;
+	duration = $("#edit-input").val() * 60;
 	$("#timer").text($("#edit-number").text() + ":" + "00" + ":");
 	$("#timer1").text("00");
 	timer_on = 0;
 });
 
 $("#edit-input").hide();
+$("#message").hide();
 
 $("#edit-a").click(function () {
+	if (timer_on == 1) {
+		$("#message").show();
+		$("#message").text("Reset the timer!!");
+		$("#message").fadeOut(3000);
+		return;
+	}
+	edit = true;
 	$("#edit-input").toggle();
 	$("#edit-input").keyup(function (e) {
 		var code = e.which;
@@ -62,7 +71,8 @@ $("#edit-a").click(function () {
 				$("#edit-number").text("00");
 			} else {
 				$("#edit-number").text($("#edit-input").val());
-				duration = $("#edit-number").text() * 60;
+				duration = $("#edit-input").val() * 60;
+				edit = false;
 			}
 			$("#edit-input").val("");
 			$("#edit-input").hide();
