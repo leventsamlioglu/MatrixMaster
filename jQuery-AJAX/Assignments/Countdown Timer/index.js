75c874192b0db139d6f0ad3eb8ca4b050135a6ee
leventsamlioglu <leventsamlioglu@icloud.com>
2023-05-15 00:04:34 +0200

    add comments and some updates

--- jQuery-AJAX/Assignments/Countdown Timer/index.js
+++ jQuery-AJAX/Assignments/Countdown Timer/index.js
@@ -1,70 +1,81 @@
+// Initials
+
 let duration = $("#edit-number").text() * 60;
 let edit = false;
-let milli = 100;
+let milliseconds = 100;
 let timeout, timeout1;
 let timer_on = 0;
-var minutes, seconds, milliseconds;
+let minutes, seconds;
+$("#edit-input").hide();
+$("#message").hide();
+
+// Minutes and seconds counter
 
 function timedCount() {
-	$("#edit-input").hide();
+	$("#edit-input").hide(); // Hide input
 	edit = false;
-	if (duration == 0) {
+	if (duration == 0) { // If count process finished
 		clearTimeout(timeout);
 		clearTimeout(timeout1);
-		duration = $("#edit-number").text() * 60;
+		duration = $("#edit-number").text() * 60; // Set duration
 		return;
 	}
 	duration--;
-	minutes = parseInt(duration / 60, 10);
+	minutes = parseInt(duration / 60, 10); 
 	seconds = parseInt(duration % 60, 10);
 
 	minutes = minutes < 10 ? "0" + minutes : minutes;
 	seconds = seconds < 10 ? "0" + seconds : seconds;
 
-	$("#timer").text(minutes + ":" + seconds + ":");
+	$("#timer").text(minutes + ":" + seconds + ":"); // Show minutes and seconds
 
-	timeout = setTimeout(timedCount, 1000);
+	timeout = setTimeout(timedCount, 1000); // Execute timedCount in every 1 sec
 }
 
-function countMili() {
-	if (edit) return;
-	milliseconds = milli < 10 ? "0" + milli : milli;
-	$("#timer1").text(milliseconds);
-	milli == 0 ? (milli = 100) : milli--;
-	timeout1 = setTimeout(countMili, 10);
+// Milliseconds counter 
+
+function countMilli() {
+	if (edit) return; 
+	milliseconds = milliseconds < 10 ? "0" + milliseconds : milliseconds;
+	$("#timer1").text(milliseconds); // Show milliseconds
+	milliseconds == 0 ? (milliseconds = 100) : milliseconds--;
+	timeout1 = setTimeout(countMilli, 10); // Execute countMilli in every 10 ms
 }
+
+// Start button
 
 $("#start").click(function () {
 	if (!timer_on) {
 		timer_on = 1;
 		timedCount();
-		countMili();
+		countMilli();
 	}
 });
+
+// Reset button
 
 $("#reset").click(function () {
 	clearTimeout(timeout);
 	clearTimeout(timeout1);
-	$("#edit-input").hide();
-	milli = 100;
-	duration = $("#edit-number").text() * 60;
-	$("#timer").text($("#edit-number").text() + ":" + "00" + ":");
-	$("#timer1").text("00");
+	$("#edit-input").hide(); // Hide input
+	milliseconds = 100;
+	duration = $("#edit-number").text() * 60; // Set duration
+	$("#timer").text($("#edit-number").text() + ":" + "00" + ":"); // Show minutes and seconds
+	$("#timer1").text("00"); // Show milliseconds
 	timer_on = 0;
 	edit = false;
 });
 
-$("#edit-input").hide();
-$("#message").hide();
+// Editing timer
 
 $("#edit-a").click(function () {
-	$("#edit-input").val("");
+	$("#edit-input").val(""); // Clear input value
 	if (edit == true) {
-		$("#edit-input").hide();
+		$("#edit-input").hide(); // Hide input
 		edit = false;
 		return;
 	}
-	if (timer_on == 1) {
+	if (timer_on == 1) { // If timer works at this moment
 		$("#message").show();
 		$("#message").text("Reset the timer!!");
 		$("#message").fadeOut(3000);
@@ -72,21 +83,22 @@ $("#edit-a").click(function () {
 	}
 	edit = true;
 	$("#edit-input").toggle();
-	$("#edit-input").keyup(function (e) {
-		var code = e.which;
-		if (code == 13) {
+	$("#edit-input").keyup(function (e) { // If pressed to any key in input
+		let code = e.which;
+		if (code == 13) { // If pressed 'enter' key
 			e.preventDefault();
-			if ($("#edit-input").val() < 1 || $("#edit-input").val() > 25) {
+			// Check entered value < 1 or value > 25 
+			if ($("#edit-input").val() < 1 || $("#edit-input").val() > 25) { 
 				$("#message").show();
 				$("#message").text("Number must be between 1-25!!");
 				$("#message").fadeOut(3000);
 				return;
-			} else {
+			} else { // if value is between 1-25
 				$("#edit-number").text($("#edit-input").val());
-				duration = $("#edit-input").val() * 60;
+				duration = $("#edit-input").val() * 60; // Set entered value as duration
 				edit = false;
 			}
-			$("#edit-input").hide();
+			$("#edit-input").hide(); // Hide input
 		}
 	});
 });
