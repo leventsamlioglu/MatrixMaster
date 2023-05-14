9b515f1eff8a46c6ba3747382c8c1244662ab31a
leventsamlioglu <leventsamlioglu@icloud.com>
2023-05-14 09:45:02 +0200

    add functionality

--- jQuery-AJAX/Assignments/Countdown Timer/index.js
+++ jQuery-AJAX/Assignments/Countdown Timer/index.js
@@ -1,47 +1,71 @@
-let duration = 25 * 60;
-let duration1 = 25 * 60 * 60
-let timeout,timeout1;
-let timer_on = 0;
-var minutes, seconds, miliseconds;
+let duration = $("#edit-number").text() * 60;
 
+let milli = 100;
+let timeout, timeout1;
+let timer_on = 0;
+var minutes, seconds, milliseconds;
 
 function timedCount() {
-    minutes = parseInt(duration / 60, 10);
-  seconds = parseInt(duration % 60, 10);
-
-minutes = minutes < 10 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-	$("#timer").text(minutes + ":" + seconds + ':');
+	if (duration == 0) {
+		clearTimeout(timeout);
+		clearTimeout(timeout1);
+		duration = $("#edit-number").text() * 60;
+        
+		return;
+	}
 	duration--;
+	minutes = parseInt(duration / 60, 10);
+	seconds = parseInt(duration % 60, 10);
+
+	minutes = minutes < 10 ? "0" + minutes : minutes;
+	seconds = seconds < 10 ? "0" + seconds : seconds;
+
+	$("#timer").text(minutes + ":" + seconds + ":");
+
 	timeout = setTimeout(timedCount, 1000);
 }
 
-function countMili(){
-    miliseconds = parseInt(duration1%100, 10);
-    miliseconds = miliseconds < 10 ? '0' + miliseconds: miliseconds
-    duration1--
-	$("#timer1").text(miliseconds);
-
-    timeout1 = setTimeout(countMili,10)
-
+function countMili() {
+	milliseconds = milli < 10 ? "0" + milli : milli;
+	$("#timer1").text(milliseconds);
+	milli == 0 ? (milli = 100) : milli--;
+	timeout1 = setTimeout(countMili, 10);
 }
 
 $("#start").click(function () {
 	if (!timer_on) {
 		timer_on = 1;
-        countMili()
 		timedCount();
+		countMili();
 	}
 });
 
 $("#reset").click(function () {
 	clearTimeout(timeout);
 	clearTimeout(timeout1);
-
-	duration = 25*60;
-	duration1 = 25*60;
+	milli = 100;
+	duration = $("#edit-number").text() * 60;
+	$("#timer").text($("#edit-number").text() + ":" + "00" + ":");
+	$("#timer1").text("00");
 	timer_on = 0;
 });
 
+$("#edit-input").hide();
 
-
+$("#edit-a").click(function () {
+	$("#edit-input").toggle();
+	$("#edit-input").keyup(function (e) {
+		var code = e.which;
+		if (code == 13) {
+			e.preventDefault();
+			if ($("#edit-input").val() == (NaN || 0)) {
+				$("#edit-number").text("00");
+			} else {
+				$("#edit-number").text($("#edit-input").val());
+				duration = $("#edit-number").text() * 60;
+			}
+			$("#edit-input").val("");
+			$("#edit-input").hide();
+		}
+	});
+});
