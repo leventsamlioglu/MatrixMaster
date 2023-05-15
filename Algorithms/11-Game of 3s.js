7e072ae3122f224fd80d15e4564e1bb7993b8082
leventsamlioglu <leventsamlioglu@icloud.com>
2023-05-15 16:46:25 +0200

    change code

--- Algorithms/11-Game of 3s.js
+++ Algorithms/11-Game of 3s.js
@@ -6,46 +6,45 @@
 // If it's not, either add 1 or subtract 1 (to make it divisible by 3), then divide it by 3.
 // The game stops when you reach "1".
 
-
 // Pseudocode:
 // 1- Create a function that takes a parameter as a number
-// 2- Loop through from the number to 1
-// 3- Check if item is divisible by 3
-// 4- If so, print number and 0
-// 5- Else if number + 1 is divisible by 3, print number and 1
-// 6- Else if number -1 is divisible by 3, print number and -1
-
+// 2- Create an empty array
+// 3- Create a while loop and check if number is not equal to 1
+// 4- 	Inside the loop, check if number divisible by 3
+// 5- 		If so, push number to array
+// 6- 		Set the number to number/3
+// 7- 	Else, 
+// 8- 		define a variable and if modulus of the number equals to 1 then send -1 to the variable, else send -1
+// 9- 		push the number and the variable to array
+// 10- 		number = (number + variable)/3
+// 11-  Push 1 to array as the last item
+// 12-  Convert array to string
+// 13- Call the function
 
 // Code:
 
-function totalNumber(number) {
-	for (let i = number; i >= 1; i--) {
-		if (i === 1) {
-			console.log(i);
-		} else if (i % 3 === 0) {
-			console.log(i + " 0");
-		} else if ((i + 1) % 3 === 0) {
-			console.log(i + " 1");
-		} else if ((i - 1) % 3 === 0) {
-			console.log(i + " -1");
+function gameOf3s(number) {
+	const array = [];
+	while (number !== 1) {
+		if (number % 3 === 0) {
+			array.push(`${number}`);
+			number = number / 3;
+		} else {
+			let nextSteps = number % 3 === 1 ? -1 : 1;
+			array.push(`${number} ${nextSteps}`);
+			number = (number + nextSteps) / 3;
 		}
 	}
+	array.push("1");
+	return array.join("\n");
 }
-
-totalNumber(10);
-
+console.log(gameOf3s(100));
 
 // Diagram:
 
-//  i	i === 1     i % 3 === 0	    (i + 1) % 3 === 0	(i - 1) % 3 === 0	Result
-// 10	false 	    false 	        false 	            true 	            10  -1
-//  9	false 	    true 	        false 	            false 	            9  0
-//  8	false 	    false 	        true 	            false 	            8 +1
-//  7	false 	    false 	        false 	            true 	            7  -1
-//  6	false 	    true 	        false 	            false 	            6  0
-//  5	false 	    false 	        true 	            false 	            5 +1
-//  4	false 	    false 	        false 	            true 	            4  -1
-//  3	false 	    true 	        false 	            false 	            3  0
-//  2	false 	    false 	        true 	            false 	            2 +1
-//  1	true 	    false 	        false 	            false 	            1
-
+//  number		array     					number		nextSteps	    array   					  number		
+//   100		[] 	     		 	 		 100 	       -1 	        [100 -1]    	  				33            
+//    33		[100 -1] 	  		  		 33 	        - 	        [100 -1, 33] 	  				11          
+//    11		[100 -1,33]   		  		 11			    1			[100 -1, 33, 11 1]	 			 4
+//     4		[100 -1, 33, 11 1]	   		 4		       -1			[100 -1, 33, 11 1, 4 -1]		 1
+// 	   1			=>				push 1 to array			=>			[100 -1, 33, 11 1, 4 -1, 1]		 
