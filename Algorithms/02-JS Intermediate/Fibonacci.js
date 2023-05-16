91b24c31f1603b396db3d06951b773f5832a6284
leventsamlioglu <leventsamlioglu@icloud.com>
2023-05-16 10:27:07 +0200

    modify the code

--- Algorithms/02-JS Intermediate/Fibonacci.js
+++ Algorithms/02-JS Intermediate/Fibonacci.js
@@ -2,9 +2,12 @@
 
 // Pseudocode:
 // 1- Create an array with values [0,1]
-// 2- Loop through numbers from 1 to 10
-// 3- Inside the loop sum i(nth) + i+1(nth) and push the sum
-// 4- Use console to print array
+// 2- Declare a function (number)
+// 3- Check if the number < 1, then return 'text'
+// 4- Else loop through numbers from 1 to number-2
+// 5- Inside the loop sum i(nth) + i+1(nth) and push the sum
+// 6- Return array
+// 7- Call the function
 
 // Code:
 let arr = [0, 1]
@@ -14,7 +17,7 @@ function fibonacci(number) {
         return ('Number must be greater then 1')
 
     } else {
-        for (let i = 0; i < number; i++) {
+        for (let i = 0; i < number-2; i++) {
             arr.push(arr[i] + arr[i + 1])
         }
         return arr
@@ -29,4 +32,4 @@ console.log(fibonacci(10))
 // 2: i=1 -> Result: [0,1,1,2]
 // 3: i=2 -> Result: [0,1,1,2,3]
 // ...
-// 10: i=10 -> Result:[0,1,1,2,3,5,8,13,21,34,55,89]
+// 8: i=8 -> Result:[0,1,1,2,3,5,8,13,21,34]
