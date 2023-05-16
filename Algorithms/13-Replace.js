984cc21c1c70350babe5ce69b004d70bd579643e
leventsamlioglu <leventsamlioglu@icloud.com>
2023-05-16 12:36:31 +0200

    update the code

--- Algorithms/13-Replace.js
+++ Algorithms/13-Replace.js
@@ -1,48 +1,33 @@
 // Perform a search and replace on the sentence using the arguments provided and return the new sentence.
 
 // Pseudocode:
-// 1- Create a function
-// 2-   convert string to array
-// 3-   loop through 0 to length of the array
-// 4-       check if has the array the searching word
-// 5-       if so, replace the new word
-// 6-       replace the new word
-// 7-   convert array to string
-// 8-   return string
+// 1- Create a function with 3 parameters (str,before,after)
+// 2-   define a variable1 and assign 'before' with the first letter of 'before' is capitalized
+// 3-   define a variable2 and assign 'after' with the first letter of 'after' is capitalized
+// 4-   check if before = variable1 
+// 5-       if so, replace the variable1 with before in the string and return it
+// 6-       else, replace the lowerCased 'after' with before in the string and return it
+// 7- Call the function with 3 parameter and print it
+
 
 function myReplace(str, before, after) {
-	let arr = str.split(" ");
-console.log(arr);	
-for (let i = 0; i < arr.length; i++) {
-		if (arr[i] === before) {
-			// arr.splice(i, 1, after);
-            arr[i]=after
-		}
+	let capitalizeBefore = before[0].toUpperCase() + before.substring(1);
+	let capitalizeAfter = after[0].toUpperCase() + after.substring(1);
+
+	if (before === capitalizeBefore) {
+		return str.replace(before, capitalizeAfter);
+	} else {
+		return str.replace(before, after.toLowerCase());
 	}
-	str = arr.join(" ");
-	return console.log(str);
 }
 
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
-
+console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "Leaped")); 
 
 // Diagram:
 
-// str	"A quick brown fox jumped over the lazy dog"		
-// before	jumped		
-// after	leaped		
-// arr	[‘A’, ‘quick’, ‘brown’, ‘fox’, ‘jumped’, ‘over’, ‘the’, ‘lazy’, ‘dog’]	
+// str	"A quick brown fox jumped over the lazy dog"
 
-// i	arr[i]	    arr[i] === before	    arr[i]
-// 0	A	        false 	                A
-// 1	quick	    false 	                quick
-// 2	brown	    false 	                brown
-// 3	fox	        false 	                fox
-// 4	jumped	    true 	                leaped
-// 5	over	    false 	                over
-// 6	the	        false 	                the
-// 7	lazy	    false 	                lazy
-// 8	dog	        false 	                dog
-			
-// result	"A quick brown fox leaped over the lazy dog"		
-
+// before	after		capitalizeBefore	  capitalizeAfter  if statement					str
+//----------------------------------------------------------------------------------------------------------------------
+// jumped	Leaped		Jumped				  Leaped			false		"A quick brown fox leaped over the lazy dog"
+// Jumped	leaped		Jumped				  Leaped			true		"A quick brown fox Leaped over the lazy dog"
