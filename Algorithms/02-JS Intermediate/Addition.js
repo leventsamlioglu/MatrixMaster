c5d5116c9ed565c1d66fe0c59662b09ce674b71a
leventsamlioglu <leventsamlioglu@icloud.com>
2023-05-17 13:32:42 +0200

    update code

--- Algorithms/02-JS Intermediate/Addition.js
+++ Algorithms/02-JS Intermediate/Addition.js
@@ -3,14 +3,14 @@
 // Pseudocode:
 // 1- Create a variable named 'total'
 // 2- Loop through numbers from 200 until 2700
-// 3- Check if value is divided by 3 or 5
+// 3- Check if value is divided by 3 or 5 but not both 3 and 5
 // 4- If then add value to total
 
 // Code:
 
 let total = 0;
 for (let i = 200; i <= 2700; i++) {
-	if ((i % 3 === 0 || i % 5 === 0) && (i % 3 === 0 && i % 5 === 0)) {
+	if ((i % 3 === 0 || i % 5 === 0) && !(i % 3 === 0 && i % 5 === 0)) {
 		total += i;
 	}
 }
@@ -21,5 +21,5 @@ console.log(total);
 // 2: i++ ... i=201 -> Total:401
 // 3: i++ ... i=202 -> Total:401
 // ...
-// 2500: i++ ... i=2700 -> Total:242985
+// 2500: i++ ... i=2700 -> Total:13467
 // 2501: i++ ... 2701 is not smaller than 2700 -> Loop will stop
