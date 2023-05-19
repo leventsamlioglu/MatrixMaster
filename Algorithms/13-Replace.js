c765bdf8d370773ffb5fff9a894e935fdf13dcbd
leventsamlioglu <leventsamlioglu@icloud.com>
2023-05-19 10:19:53 +0200

    update code

--- Algorithms/13-Replace.js
+++ Algorithms/13-Replace.js
@@ -2,26 +2,24 @@
 
 // Pseudocode:
 // 1- Create a function with 3 parameters (str,before,after)
-// 2-   define a variable1 and assign 'before' with the first letter of 'before' is capitalized
-// 3-   define a variable2 and assign 'after' with the first letter of 'after' is capitalized
-// 4-   check if before = variable1 
-// 5-       if so, replace the variable1 with before in the string and return it
+// 3-   define a variable and assign 'after' with the first letter of 'after' is capitalized
+// 4-   check if "A" < before[0] < "Z"
+// 5-       if so, replace the variable with before in the string and return it
 // 6-       else, replace the lowerCased 'after' with before in the string and return it
 // 7- Call the function with 3 parameter and print it
 
 
 function myReplace(str, before, after) {
-	let capitalizeBefore = before[0].toUpperCase() + before.substring(1);
 	let capitalizeAfter = after[0].toUpperCase() + after.substring(1);
-
-	if (before === capitalizeBefore) {
+	
+	if ('A' <= before[0] && before[0]<= 'Z') {
 		return str.replace(before, capitalizeAfter);
 	} else {
 		return str.replace(before, after.toLowerCase());
 	}
 }
 
-console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "Leaped")); 
+console.log(myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped")); 
 
 // Diagram:
 
