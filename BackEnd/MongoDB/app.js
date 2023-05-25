13629361ea07ecbfbd51ed1d503821bce89a0532
leventsamlioglu <leventsamlioglu@icloud.com>
2023-05-25 09:51:28 +0200

    connect to DB

--- BackEnd/MongoDB/app.js
+++ BackEnd/MongoDB/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const exportedRoutes = require("./Routes/routes");
+const mongoose = require("mongoose");
 
 // express app
 const app = express();
+
+// connect to MongoDB
+const dbURI = "mongodb+srv://Leo:test1122@blog.u0kfsp6.mongodb.net/";
+mongoose
+	.connect(dbURI)
+	.then((result) => console.log('Successfully connected to DB!'))
+	.catch((err) => console.log(err));
 
 // listen for requests
 app.listen(3000);
