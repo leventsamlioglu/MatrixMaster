5ec4b402235cc47de2be19db3d625f0d7a2d3a3e
leventsamlioglu <leventsamlioglu@icloud.com>
2023-05-30 22:15:20 +0200

    add server js

--- BackEnd/Demo Challenge/server.js
+++ BackEnd/Demo Challenge/server.js
@@ -13,6 +13,9 @@ mongoose.connect(dbURI)
     console.log('Connected to DB...');
 })
 
+app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.use(exportedRoutes)
