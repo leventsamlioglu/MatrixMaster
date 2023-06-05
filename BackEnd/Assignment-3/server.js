b1ef016a0ed78f08ddb976dbc4919ae702d98fd4
leventsamlioglu <leventsamlioglu@icloud.com>
2023-06-05 17:04:58 +0200

    add server js

---
+++ BackEnd/Assignment-3/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const exportedRoutes = require("./routes/routes");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const { checkUser } = require("./middleware/authMiddleware");
+require("dotenv").config();
+
+const app = express();
+
+const dbURI = process.env.URI;
+
+mongoose
+	.connect(dbURI)
+	.then(() => {
+		console.log("Successfully connected to DB!");
+		app.listen(3000);
+	})
+	.catch((err) => console.log(err));
+
+// Middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.set("view engine", "ejs");
+app.use(exportedRoutes);
