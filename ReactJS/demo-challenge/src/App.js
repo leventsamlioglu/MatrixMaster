6adc26836a32959331182121741347ddfe0d9e3e
leventsamlioglu <leventsamlioglu@icloud.com>
2023-06-27 12:29:48 +0200

    add App comp

---
+++ ReactJS/demo-challenge/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FindPlace from "./components/FindPlace";
+
+function App() {
+	return (
+		<div className="App">
+			<FindPlace />
+		</div>
+	);
+}
+
+export default App;
