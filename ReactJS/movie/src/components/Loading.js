6d3a40628338e0643b2b97191af0899af7364c0c
leventsamlioglu <leventsamlioglu@icloud.com>
2023-06-28 22:07:33 +0200

    add loading spinner

---
+++ ReactJS/movie/src/components/Loading.js
@@ -0,0 +1,11 @@
+const Loading = () => {
+	return (
+		<div className="d-flex justify-content-center mt-5">
+			<div className="spinner-border text-dark" role="status">
+				<span className="sr-only">Loading...</span>
+			</div>
+		</div>
+	);
+};
+
+export default Loading;
--- ReactJS/movie/src/components/Movies.js
+++ ReactJS/movie/src/components/Movies.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import CardComp from "./CardComp";
 import FormComp from "./FormComp";
+import Loading from "./Loading";
 
 class Movies extends React.Component {
 	constructor(props) {
@@ -9,6 +10,7 @@ class Movies extends React.Component {
 		this.state = {
 			searchItem: "",
 			movies: [],
+			loading: false,
 		};
 	}
 	
@@ -17,14 +19,19 @@ class Movies extends React.Component {
 	};
 
 	clickHandle = () => {
-		axios
-			.get(
-				`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&query=${this.state.searchItem}`
-			)
-			.then((result) => {
-				const movies = result.data.results;
-				this.setState({ movies: movies, searchItem: "" });
-			});
+		this.setState({loading:true})
+		setTimeout(() => {
+			
+			axios
+				.get(
+					`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&query=${this.state.searchItem}`
+				)
+				.then((result) => {
+					const movies = result.data.results;
+					this.setState({ movies: movies, searchItem: "", loading:false });
+	
+				});
+		}, 1000);
 	};
 
 	handleKeyPress = (e) => {
@@ -49,11 +56,11 @@ class Movies extends React.Component {
 					click={this.clickHandle}
 					value={this.state.searchItem}
 				/>
-				<div className="d-flex flex-wrap">
+				{this.state.loading ? <Loading/>:<div className="d-flex flex-wrap">
 					{this.state.movies.map((movie) => {
 						return <CardComp key={movie.id} movie={movie} />;
 					})}
-				</div>
+				</div>}
 			</div>
 		);
 	}
