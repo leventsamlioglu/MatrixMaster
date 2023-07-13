30b9ee752a8868dd7b2335e464cfc52af53891eb
leventsamlioglu <leventsamlioglu@icloud.com>
2023-07-13 12:46:19 +0200

    insert values into tables

---
+++ SQL/2-Yelp/6.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+    restaurants(name)
+VALUES
+    ('Sera Di Luna'),
+    ('Pand 83'),
+    ('New Blue Lotus'),
+    ('Family Nieuw-Lekkerland'),
+    ('Eethuis de Molen'),
+    ('Sfinx'),
+    ('La Pizza'),
+    ('Partycentrum de Klok'),
+    ('Buena Vista'),
+    ('Cipriano');
---
+++ SQL/2-Yelp/7.sql
@@ -0,0 +1,114 @@
+INSERT INTO
+    reviews(user_id, restaurant_id, content, rating)
+VALUES
+    (
+        1,
+        4,
+        'Echt niet alleen honderdduizenden toeristen kunnen dit waarderen, ook voor ons, de gewone Hollander die bovendien nog in de buurt woont, blijft het een prachtgezicht, die 19 molens in dat wijdse en weide landschap. We zijn er met de Waterbus en per fiets naar toe gegaan. Even opgefrist op een leuk loungeterras en dan de tocht langs alle molens gemaakt die je overigens ook te voet of per boot kunt doen. Een echte aanrader.',
+        9
+    ),
+    (
+        1,
+        3,
+        'Woon al een tijdje in Rotterdam maar was eigenlijk nooit bij Bazaar geweest. Iets waar verandering in moest komen. Met partner dus even wezen ontbijten. Twee keer ontbijt besteld. Minder dan 8 minuten al op tafel, heerlijke service en lekker vlot! Eten was erg lekker, je krijgt veel voor je geld. Leuke sfeer met lichtjes en kaarsjes.',
+        8
+    ),
+    (
+        2,
+        2,
+        'Zeer lekker! Aanrader Nahár large lunch! Gezellige setting, net een Bazar in Marrakech. Bediening is snel & efficiënt. Bij volgende citytrip zeker opnieuw te bezoeken!',
+        7
+    ),
+    (
+        2,
+        10,
+        'Een eethuis met Turkse, Marokkaanse en Iraanse gerechten. Ik heb hier vanavond erg lekker gegeten. Ik had de Pilic Sis Kebab besteld (kipspies) en dit gerecht wordt geserveerd met saffraanrijst en gebakken aardappelen in hun schilletje. De kip was erg mals en sappig. De marinade is heerlijk kruidig. Wat me ook positief opvalt is de porties die je krijgt; als grote eter hoef je zelfs geen voorgerecht te nemen. ',
+        10
+    ),
+    (
+        3,
+        1,
+        'Kom hier graag terug mits er een goede plek beschikbaar is!',
+        2
+    ),
+    (
+        4,
+        8,
+        'Tijd geleden met collegas bij dit restaurant gegeten. Erg gezellig en goed veel eten. Vanwege de drukte duurde de bediening soms wat langer dan normaal, daar hadden wij begrip voor.',
+        6
+    ),
+    (4, 7, 'Prijs kwaliteit ook prima!', 7),
+    (
+        4,
+        3,
+        'Hier kom ik elke keer als ik in Rotterdam ben. Het is de moeder van de Bazar eethuizen. We hebben er inmiddels al een paar jaar ook een in Amsterdam maar dit was de eerste.
+Ik kom hier graag. ',
+        7
+    ),
+    (
+        5,
+        9,
+        'Bij elke bestelling zie je de ogen van de meeste mensen meteen weer een stuk groter worden gevuld met een lekkere trek - het eten wordt namelijk in niet al te zuinige porties geserveerd.',
+        8
+    ),
+    (
+        5,
+        3,
+        'Heerlijk gegeten in eethuis Bazar. Het restaurant past goed in de Witte de Withstraat. Ik kom hier graag nog een keer terug.',
+        3
+    ),
+    (
+        6,
+        4,
+        'Tip: de thee wordt praktisch zwart geserveerd (er zit geen suiker in). Ze hebben geen theeselectie, dus als je hier niet van houdt, bestel dan desnoods heet water met muntbladeren (als je muntthee besteld krijg je zwarte thee met muntbladeren).',
+        4
+    ),
+    (
+        6,
+        1,
+        'Heel originele locatie: oosterse kitsch. Heel lekker eten  voor weinig geld (hoofdgerechten tussen 9 en 16 euro). Lekkere ribben, heerlijke vleesspiezen en kebabs met sappige rijst en kruiden,...
+Het is een hele grote en drukke zaak, maar alles loopt gesmeerd.',
+        5
+    ),
+    (
+        7,
+        7,
+        'Het personeel is heel vriendelijk, maar als het druk is (en dat is het vaak) dan laat het zich soms wat te wensen over. Maar de sfeer is zo goed dat het de pret niet mag drukken. Voor een bijzondere avond uit is Bazar een uitstekende plek!',
+        7
+    ),
+    (
+        7,
+        10,
+        'Een fijn restaurant, mooie zuiderse lampen, gezellige sfeer, lekkere schotels en rijkelijk gevuld. Vriendelijke maar chaotische bediening. Zeer goede prijzen!',
+        10
+    ),
+    (
+        8,
+        2,
+        'Zo kun je, zeker met een gezelschap, voor relatief weinig geld een zeer gezellige avond hier hebben waarbij je er redelijk op kunt vertrouwen dat iedereen zijn gerecht wel kan waarderen.',
+        8
+    ),
+    (
+        9,
+        9,
+        'Het was een paar jaar geleden dat ik hier had gegeten. Het eten en de service viel erg tegen. Het eten is niet echt warm en het smaakt ook niet bijzonder, beetje flauw. Het terras ziet er gezellug uit, misschien hadden we beter alleen drankje kunnen doen op het terras.',
+        5
+    ),
+    (
+        9,
+        6,
+        'Ik kan hier heel kort over zijn. Verwacht er niet teveel van want dan kan je echt teleurgesteld raken. Ik heb soms het gevoel en idee  dat Bazar zijn beste tijd heeft gehad.',
+        6
+    ),
+    (
+        9,
+        3,
+        'Goed. Positief beginnen: het eten bij de Bazar is altijd lekker en van goede kwaliteit. Maar nu dan toch negatief: het is niet meer wat het geweest is. Wij hadden allemaal het idee dat de porties kleiner zijn geworden. We zaten in een nis, waar het personeel gewoon niet kwam bedienen. We voelden ons echt behoorlijk genegeerd. Daarnaast was het personeel gewoon niet vriendelijk. Zelfs de dame bij de kassa vroeg geeneens meer "En heeft alles gesmaakt?" of "Was alles naar wens?"',
+        8
+    ),
+    (
+        10,
+        6,
+        'Wat het menu betreft kan ik niet veel informatie geven, omdat ik al jaren een favoriet heb en misschien een enkele keer wat anders heb geprobeerd, maar toch altijd weer terug ga naar mn favoriet om mezelf echt blij te maken. ',
+        9
+    );
