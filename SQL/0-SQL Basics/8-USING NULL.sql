0e07d28654413042916fea29249fecd9564812d0
leventsamlioglu <leventsamlioglu@icloud.com>
2023-07-25 11:23:33 +0200

    right join

--- SQL/0-SQL Basics/8-USING NULL.sql
+++ SQL/0-SQL Basics/8-USING NULL.sql
@@ -2,9 +2,15 @@
 SELECT name FROM teacher WHERE dept IS NULL
 
 # 2. Note the INNER JOIN misses the teachers with no department and the departments with no teacher. 
-SELECT teacher.name, dept.name FROM teacher INNER JOIN dept ON (teacher.dept=dept.id)
+SELECT teacher.name, dept.name 
+  FROM teacher INNER JOIN dept ON (teacher.dept=dept.id)
 
 # 3. Use a different JOIN so that all teachers are listed. 
 SELECT teacher.name, dept.name
   FROM teacher LEFT JOIN dept
+    ON (teacher.dept = dept.id)
+
+# 4. Use a different JOIN so that all departments are listed. 
+SELECT teacher.name, dept.name
+  FROM teacher RIGHT JOIN dept
     ON (teacher.dept = dept.id)
