e5089da1aed335ed5ef663d842c9a796ec6e2847
leventsamlioglu <leventsamlioglu@icloud.com>
2023-07-29 22:41:50 +0200

    basic SELECT

---
+++ SQL/0-SQL Basics/9-SELF JOIN.sql
@@ -0,0 +1,15 @@
+-- # 1. How many stops are in the database. 
+SELECT COUNT(*)
+  FROM stops
+
+-- # 2. Find the id value for the stop 'Craiglockhart' 
+SELECT id
+  FROM stops
+ WHERE name = 'Craiglockhart'
+
+-- # 3. Give the id and the name for the stops on the '4' 'LRT' service. 
+SELECT id, name
+  FROM stops JOIN route
+    ON id = stop
+ WHERE num = '4'
+   AND company = 'LRT'
