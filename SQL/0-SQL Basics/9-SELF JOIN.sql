34ac482ed794a1646de6f35aea4c6697587c515c
leventsamlioglu <leventsamlioglu@icloud.com>
2023-08-01 19:00:42 +0200

    JOIN example

--- SQL/0-SQL Basics/9-SELF JOIN.sql
+++ SQL/0-SQL Basics/9-SELF JOIN.sql
@@ -25,3 +25,11 @@ SELECT a.company, a.num, a.stop, b.stop
   FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
 WHERE a.stop=53 AND b.stop = 149
+
+-- # 6. Change the query so that the services between 'Craiglockhart' and 'London Road' are shown.
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name = 'London Road'
