8dc2ed80ec18c9b0c4e228e4f4c59fd48c2ee329
leventsamlioglu <leventsamlioglu@icloud.com>
2023-08-02 09:08:20 +0200

    JOIN example

--- SQL/0-SQL Basics/9-SELF JOIN.sql
+++ SQL/0-SQL Basics/9-SELF JOIN.sql
@@ -33,3 +33,9 @@ FROM route a JOIN route b ON
   JOIN stops stopa ON (a.stop=stopa.id)
   JOIN stops stopb ON (b.stop=stopb.id)
 WHERE stopa.name='Craiglockhart' AND stopb.name = 'London Road'
+
+-- # 7. Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith') 
+SELECT a.company, a.num 
+  FROM route a JOIN route b ON a.company = b.company AND a.num = b.num
+ WHERE a.stop = 115 AND b.stop = 137
+ GROUP BY a.company, a.num
